9e0390dc8cd3c000aeb2b3f2b4c09b0c08c5687e
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-20 19:24:38 -0300

    [config] components são exibidos através de router.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { ChairSvgComponent } from './shared/chair-svg/chair-svg.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, ChairSvgComponent, HeaderComponent],
   templateUrl: './app.component.html',
+  //imports: [RouterOutlet, HeaderComponent, AssertConnectionComponent],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
