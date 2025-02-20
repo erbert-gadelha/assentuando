b06e40683e834c90736c34fe6a22b07213cc36f8
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-20 16:32:08 -0300

    initial

---
+++ backend/src/main/java/com/ebgr/assentuando/AssentuandoApplication.java
@@ -0,0 +1,13 @@
+package com.ebgr.assentuando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssentuandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssentuandoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ebgr/assentuando/AssentuandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebgr.assentuando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssentuandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
