ab0f5b25a8b43b71dcd02c379e746fa6477200c4
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-21 20:25:44 -0300

    [config] definido rota para gerenciamento de sala

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { ManagementComponent } from "./feature/management/management.component";
 
 export const routes: Routes = [
     {path: '', component: HomeComponent, pathMatch: 'full'},
-    {path: 'sala', component: ManagementComponent, pathMatch: 'prefix'},
+    {path: 'sala', component: ManagementComponent, pathMatch: 'full'},
+    { path: 'sala/:id', component: ManagementComponent }
 ];
