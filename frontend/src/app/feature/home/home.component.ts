818d8b0d0bd84c3764c3a1f05110cf7bfbcbd0e1
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-21 20:26:38 -0300

    [refact] implementando responsividade por meio de grid-layout

--- frontend/src/app/feature/home/home.component.ts
+++ frontend/src/app/feature/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { CommonModule, NgFor} from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, NgFor],
+  imports: [HeaderComponent, NgFor, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
