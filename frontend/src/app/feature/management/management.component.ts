8502d00d3765da0ed5cf75196a7a63c680d84ff2
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-21 20:31:03 -0300

    [feat] interações por click com os elementos do grid

--- frontend/src/app/feature/management/management.component.ts
+++ frontend/src/app/feature/management/management.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule, NgFor} from '@angular/common';
-import { ChairSvgComponent } from '../../shared/chair-svg/chair-svg.component';
+import { NgFor, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-management',
-  imports: [NgFor, ChairSvgComponent],
+  imports: [NgFor, NgIf],
   templateUrl: './management.component.html',
   styleUrl: './management.component.scss'
 })
@@ -22,26 +21,91 @@ export class ManagementComponent {
     {id:21, name:"auditório"},
   ];
 
-  public seats:Seat[] = [
-    {id:1, person:"Fulano"}, {id:2, person:"Sicrano"}, {id:3, person:"Williano"}, {id:4, person:"Ribriano"}, {id:5, person:"Marciano"}]
-  //];
+  public seats:Seat[] = []
+  public selected:Seat|null = null
+  public focused:boolean = false
+  @ViewChild('input') input: ElementRef|undefined;
+
 
 
   ngOnInit() {
+    for (let i = 0; i < 15; i++) {
+      this.seats.push({id:1, person:null, color:"white", createdAt:null})       
+    }
+
     this.route.paramMap.subscribe(params => {
       this.id = params.get('id');
       console.log('ID:', this.id);
 
       if(this.id != null) {
         const id = Number.parseInt(this.id)
-        for (let i = 0; i < this.theathers.length; i++) {
+        for (let i = 0; i < this.theathers.length; i++)
           if(this.theathers[i].id == id) {
             this.title = this.theathers[i].name
             break;
-          }          
-        }
+          }
       }
     });
+  }
+
+  public onEnter(event: Event) {
+    this.onClick(-1);
+  }
+
+
+
+  private formatter:Intl.DateTimeFormat  = new Intl.DateTimeFormat('pt-BR', {
+    day:'2-digit', month:'2-digit', year:'numeric', hour: '2-digit', minute: '2-digit', hour12: false
+  });
+
+
+  public onMouseOver(id:number) {
+    /*console.log("onMouseOver", id);
+    console.log("focused", this.focused);
+    console.log("selected", this.selected);*/
+
+    if(this.focused)
+      return;
+
+    if(id < 0 || id >= this.seats.length)
+      this.selected = null;
+    else if(this.seats[id].person)
+      this.selected = this.seats[id];
+  }
+
+  public reset() {
+    if(this.input?.nativeElement)
+      this.input.nativeElement.value = "";
+  }
+  
+  
+  public onClick(id:number) {
+
+    if(id < 0 || id >= this.seats.length) {
+      this.focused = false;
+
+      if(this.selected != null) {
+        let input:string = this.input?.nativeElement.value.trim();
+        console.warn(`valor: "${input}"`);
+
+        if(input) {
+          this.selected.person = input;
+          this.selected.createdAt = this.formatter.format(new Date().getTime());
+          this.selected.color = "var(--color-0)"
+        } else {
+          this.selected.person = null;
+          this.selected.createdAt = null;
+          this.selected.color = "white"
+        }
+      }
+
+      this.selected = null;
+      return;
+    }
+
+    this.selected = this.seats[id];
+    this.selected.color = "gray";
+    this.focused = true;
   }
 }
 
@@ -53,4 +117,6 @@ interface Theater {
 interface Seat {
   id:number,
   person:string|null,
+  color:any,
+  createdAt:string|null
 }
