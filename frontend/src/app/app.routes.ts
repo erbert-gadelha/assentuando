24b4f7de8235bc1cc60f26374e0d447338ebc488
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-20 21:11:39 -0300

    [feat] gerada comonente de gerenciamento de cadeiras

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./feature/home/home.component";
+import { ManagementComponent } from "./feature/management/management.component";
 
 export const routes: Routes = [
     {path: '', component: HomeComponent, pathMatch: 'full'},
+    {path: 'sala', component: ManagementComponent, pathMatch: 'prefix'},
 ];
