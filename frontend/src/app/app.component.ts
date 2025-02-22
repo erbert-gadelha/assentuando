b20dd96598d23e58d74bb49f360dec3bbfb90027
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-22 15:49:34 -0300

    [config] injetando dependencias HTTP.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './feature/home/home.component';
-import { ChairSvgComponent } from './shared/chair-svg/chair-svg.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ChairSvgComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   //imports: [RouterOutlet, HeaderComponent, AssertConnectionComponent],
   styleUrl: './app.component.scss'
